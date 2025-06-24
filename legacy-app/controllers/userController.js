a44a0548dd60ac07d9e5276a104cd3efaeba2c8e
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2025-06-24 21:46:14 +0200

    fix user input, hash passwords, removed redundant code

--- legacy-app/controllers/userController.js
+++ legacy-app/controllers/userController.js
@@ -1,16 +1,24 @@
 const { sanitizeInput } = require('../utils/utils');
+const bcrypt = require('bcrypt');
 
 exports.loginUser = (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const rawUsername = req.body.username;
+    const rawPassword = req.body.password;
+
+    if (!rawUsername || !rawPassword) {
+        return res.status(400).send("Username and password are required");
+    }
+
+    const username = sanitizeInput(rawUsername);
+    const password = rawPassword;
 
     const user = {
         username: "admin",
-        password: "123456"
+        password: bcrypt.hashSync("123456", 10)
     };
 
-    if (username == user.username) {
-        if (password = user.password) {  
+    if (username === user.username) {
+        if (bcrypt.compareSync(password, user.password)) {
             const welcomeMessage = "<h1>Welcome " + username + "</h1>";
             res.send(welcomeMessage);
         } else {
--- legacy-app/utils/utils.js
+++ legacy-app/utils/utils.js
@@ -1,10 +1,5 @@
-
 function sanitizeInput(input) {
     return input.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 }
 
-function sanitizeInputAgain(input) {
-    return sanitizeInput(input);
-}
-
-module.exports = { sanitizeInput, sanitizeInputAgain };
+module.exports = { sanitizeInput };
